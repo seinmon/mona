abc48d2f74156a50b1d85f14dc6cf13b6b465951
Hossein Monjezi <hossein.monjezi@live.com>
2021-11-17 21:49:18 +0100

    Add setup.py
    It is a basic version and is still incomplete.

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='monalyza',
+      version='0.0.1',
+      description='Lightweight resource monitoring and analysis tool.',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url='http://github.com/seinmon/monalyza',
+      author='Hossein Monjezi',
+      author_email='hossein.monjezi@stud.uni-saarland.de',
+      license='MIT',
+      packages=['monalyza', 
+               'monalyza.*'],
+      classifiers=[
+          'Environment :: Console'
+          'Development Status :: 2 - Pre-Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6'
+          'Programming Language :: Python :: 3.7'
+          'Programming Language :: Python :: 3.8'
+          'Programming Language :: Python :: 3.9'
+          'Programming Language :: Python :: 3.10'
+          'Programming Language :: Python :: 3.11'
+          'Topic :: System :: Monitoring'
+          'Topic :: Scientific/Engineering'
+          'Operating System :: OS Independent'
+          'Intended Audience :: Developers'
+      ],
+      zip_safe=False)
