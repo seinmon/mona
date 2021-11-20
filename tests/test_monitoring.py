e1d3a3cc95892918cdae08282da56518d8b4b088
Hossein Monjezi <hossein.monjezi@live.com>
2021-11-21 00:15:39 +0100

    Add placeholders for monitoring tests

---
+++ tests/test_monitoring.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import MagicMock
+
+
+class TestMonitoring(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_proc_exist(self):
+        pass
+
+    def test_proc_not_exist(self):
+        pass
+
+    def test_memory_pid_valid(self):
+        pass
+
+    def test_memory_pid_invalid(self):
+        pass
+
+    def test_cpu_pid_valid(self):
+        pass
+
+    def test_cpu_pid_invalid(self):
+        pass
