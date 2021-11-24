60630e017cc04f5f5bdbeb39fa77bd887c9cb91d
Hossein Monjezi <hossein.monjezi@live.com>
2021-11-24 13:53:28 +0100

    Write test for the initializer of Monitoring class

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -1,24 +1,43 @@
 import unittest
-# from unittest.mock import MagicMock
-from monalyza.monitoring import monitoring
+from unittest.mock import MagicMock, patch
+from monalyza.monitoring.monitoring import Monitoring
 
 
 class TestMonitoring(unittest.TestCase):
 
-    def test_proc_exist(self):
+    class MockProc:
+        pid = 1234
 
+        def name():
+            return 'FakeProcess'
+
+
+    @patch('psutil.process_iter', return_value=[MockProc])
+    def test_proc_exist(self, mock_process_iter):
+        
+        try:
+            monitoring = Monitoring(1, "FakeProcess")
+
+        except ProcessLookupError:
+            self.fail("Failed to instantiate monitoring with FakeProcess")
+
+        else:
+            # I do not know if this is the best way of testing the pid.
+            self.assertEqual(
+                monitoring.pid, mock_process_iter.return_value[0].pid)
 
     def test_proc_not_exist(self):
-        self.assertRaises(ProcessLookupError, monitoring.Monitoring, 1, "invalid_proc_name")
+        self.assertRaises(
+            ProcessLookupError, Monitoring, 1, "invalid_proc_name")
 
-    def test_memory_pid_valid(self):
-        pass
+    # def test_memory_pid_valid(self):
+    #     pass
 
-    def test_memory_pid_invalid(self):
-        pass
+    # def test_memory_pid_invalid(self):
+    #     pass
 
-    def test_cpu_pid_valid(self):
-        pass
+    # def test_cpu_pid_valid(self):
+    #     pass
 
-    def test_cpu_pid_invalid(self):
-        pass
+    # def test_cpu_pid_invalid(self):
+    #     pass
