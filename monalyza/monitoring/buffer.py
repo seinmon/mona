9da03e912580aac2006269a4793e91391fc323da
Hossein Monjezi <hossein.monjezi@live.com>
2022-01-14 13:11:17 +0100

    Delete total_data from buffer

--- monalyza/monitoring/buffer.py
+++ monalyza/monitoring/buffer.py
@@ -13,38 +13,14 @@ class Buffer:
         self.buffer_monitoring = spm.SingleProcessMonitoring(getpid()) 
         self.buffer_size = buffer_size
 
-    def total_data(self, new_data):
-        """ Add the measurements of all subprocesses every second """
-
-        last_data_index = len(self.data)
-        calculated_data = new_data
-
-        logging.debug('Ready to calculate total data.')
-        if (not isinstance(new_data[0], str)) and last_data_index > 0:
-            logging.debug('Looking for entries in the buffer.')
-
-            if self.data[last_data_index][0] / 1000 == new_data[0] / 1000:
-                logging.debug('Modifying last element in buffer')
-                calculated_data = self.data.pop()
-
-                for index, entry in enumerate(new_data):
-                    if index == 0:
-                        continue
-
-                    else:
-                        calculated_data[index] += entry
-
-        return calculated_data
-
     def append_to_buffer(self, data):
         logging.debug('Appending to buffer.')
-        self.data.append(self.total_data(data))
+        self.data.append(data)
 
         if self.buffer_monitoring.read_resource(
             read_memory=True)[1] > self.buffer_size:
                 logging.debug('Writing to file.')
                 self.write_data()
-                
 
     def write_data(self):
         with open(self.output_file, 'a+') as csv_output:
