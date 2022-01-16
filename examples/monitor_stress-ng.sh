710050e0eeaa457bd95db530f57c54c6d6ef543d
Hossein Monjezi <hossein.monjezi@live.com>
2022-01-16 11:59:00 +0100

    Update stress-ng script

--- examples/monitor_stress-ng.sh
+++ examples/monitor_stress-ng.sh
@@ -2,5 +2,5 @@
 
 OUTPUT_DIR="./output/stress-ng"
 mkdir -p $OUTPUT_DIR
-stress-ng --cpu 2 -t 60 >> $OUTPUT_DIR/output.txt &
+stress-ng --metrics --cpu 2 -t 60 >> $OUTPUT_DIR/output.txt &
 python3 monalyza.py stress-ng
