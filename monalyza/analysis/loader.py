06aa53ccd177ee698330fb4b5a557c6227a900a5
Hossein Monjezi <hossein.monjezi@live.com>
2022-04-06 15:48:33 +0200

    Combile measurements receives csv file
    Loader class has been removed. For now, only csv file is given as
    argument to the functions. Functions create dataframe, then perform the
    necessary actions.

--- monalyza/analysis/loader.py
+++ monalyza/analysis/loader.py
@@ -1,38 +1,20 @@
+""" Loads csv file, and performs a certain action on its content. """
 import logging
 import pandas as pd
-import matplotlib
 
 
-class Loader:
-
-    def __init__(self, measurements):
-        logging.debug('Initializing loader.')
-        self.data = pd.read_csv(measurements)
-
-        # Keep this empty, and populate when its needed.
-        self.all_pids = []
-
-    def get_all_columns(self):
-        """ Get the column names.
-            It is useful to see which resources are monitored. """
-        print('columns are:')
-        print(self.data.columns)
-        print('indexes are: ')
-        print(self.data.index)
+def get_dataframe_from_csv(csv):
+    """ Create dataframe based on the measurement values in csv file. """
+    logging.debug("Creating dataframe from %s file", csv)
+    return pd.read_csv(csv)
 
 
-    # def get_all_pids(self):
-    #     """ Getting all measured pids. """
-    #     logging.debug('Getting all measured pids.')
-    #     for pid in self.data['pid']:
-    #         if pid not in self.all_pids:
-    #             self.all_pids.append(pid)
-
-    def combine_measurements(self):
-        return self.data.groupby('time')[self.data.columns[2:]].sum()
-
+def combine_csv_measurements(csv, by_group):
+    """ Combine measurements that have similar values in a certain column. """
+    data = get_dataframe_from_csv(csv)
+    logging.debug("Merging values based on %s column", by_group)
+    return data.groupby(by_group)[data.columns[2:]].sum()
 
 
 if __name__ == '__main__':
-    loader = Loader('measurements_output.csv')
-    print(loader.combine_measurements())
+    print(combine_csv_measurements('measurements_output.csv', 'time'))
