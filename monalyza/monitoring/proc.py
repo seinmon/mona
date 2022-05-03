8f3ecaed4ae130aae12fb989bbe094170a62b8e4
Hossein Monjezi <hossein.monjezi@live.com>
2022-05-03 10:37:06 +0200

    Remove unnecessary if condition from proc.py

--- monalyza/monitoring/proc.py
+++ monalyza/monitoring/proc.py
@@ -4,12 +4,8 @@ import psutil
 
 def get_pid_of_process(process: str | int) -> int:
     """Find and return the defined process using its name or pid."""
-    if isinstance(process, str) and process.isdigit():
-        process = int(process)
-
-    logging.debug('Iterating over processes to find %s.', process)
     for proc in psutil.process_iter():
-        if process == proc.name() or process ==  proc.pid:
+        if process in (proc.name(), proc.pid):
             logging.debug('Process found.')
             return proc.pid
 
