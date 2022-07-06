565bbc68842dc95fb35c6e86c4cd29942950d62a
Hossein Monjezi <hossein.monjezi@live.com>
2022-07-06 12:41:17 +0200

    Update examples

--- examples/stress-ng.sh
+++ examples/stress-ng.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 stress-ng --metrics --cpu 2 -t 60 &
-python3 monalyza.py stress-ng
-
-stress-ng --metrics --cpu 2 -t 60
+python3 mona.py stress-ng
--- examples/sysbench.sh
+++ examples/sysbench.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 sysbench --test=cpu --time=60 run &
-python3 monalyza.py sysbench
-
-sysbench --test=cpu --time=60 run
+python3 mona.py sysbench
