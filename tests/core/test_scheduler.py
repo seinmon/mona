956e73521c883e9a7d6473e201f54384f3a62055
Hossein Monjezi <hossein.monjezi@live.com>
2022-07-07 23:38:38 +0200

    Simplify scheduler tests

--- tests/core/test_scheduler.py
+++ tests/core/test_scheduler.py
@@ -5,29 +5,31 @@ from mona.core.scheduler import Scheduler
 
 
 class TestScheduler(unittest.TestCase):
+    """Test the correctness of scheduler."""
 
     def setUp(self):
+        """Setup scheduler with interval of 1 second for testing"""
         self.interval = 1
         self.scheduler = Scheduler(self.interval)
 
-    def test_initializer(self):
-        self.assertFalse(self.scheduler.running)
-        self.assertEqual(self.scheduler.interval, self.interval)
-        self.assertIsNone(self.scheduler.scheduled_timer)
-
     @patch('threading.Timer')
-    def test_scheduling_with_none(self, mock_timer):
+    @patch('threading.Timer.cancel')
+    def test_scheduling_with_none(self, mock_timer, mock_cancel):
+        """Test scheduling without a task"""
         self.scheduler.schedule(None)
         self.assertFalse(self.scheduler.running)
         mock_timer.assert_not_called()
+        
+        self.scheduler.cancel_scheduler()
+        mock_cancel.assert_not_called()
+        
+        self.scheduler.running = True
+        self.scheduler.cancel_scheduler()
+        mock_cancel.assert_not_called()
 
-    @patch('threading.Timer')
-    def test_scheduling_with_function(self, mock_timer):
-        self.scheduler.schedule(())
-        self.assertTrue(self.scheduler.running)
-        mock_timer.assert_called_once_with(self.interval, (), {})
-
-    def test_scheduled_function(self):
+    @patch('threading.Timer.cancel')
+    def test_scheduled_function(self, mock_cancel):
+        """Test scheduling with a task"""
         mock_monitoring_function = MagicMock()
         self.scheduler.schedule(mock_monitoring_function)
         self.assertTrue(self.scheduler.running)
@@ -37,24 +39,5 @@ class TestScheduler(unittest.TestCase):
 
         mock_monitoring_function.assert_called_once()
 
-    @patch('threading.Timer.cancel')
-    def test_cancel_not_running_not_scheduled(self, mock_cancel):
         self.scheduler.cancel_scheduler()
-        mock_cancel.assert_not_called()
-
-    @patch('threading.Timer.cancel')
-    def test_cancel_not_running_scheduled(self, mock_cancel):
-        self.scheduler.cancel_scheduler()
-        mock_cancel.assert_not_called()
-
-    @patch('threading.Timer.cancel')
-    def test_cancel_runnin_not_scheduled(self, mock_cancel):
-        self.scheduler.running = True
-        self.scheduler.cancel_scheduler()
-        mock_cancel.assert_not_called()
-
-    @patch('threading.Timer.cancel')
-    def test_cancel(self, mock_cancel):
-        self.scheduler.running = True
-        self.scheduler.cancel_scheduler()
-        mock_cancel.assert_not_called()
+        mock_cancel.assert_called_once()
